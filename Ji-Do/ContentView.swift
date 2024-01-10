3b1580b1f3bd11d1abc2cb6be9743b3475d18296
0Hooni <thddudgns972@gmail.com>
2024-01-10 21:27:46 +0900

    Initialize project
    SwiftUI project

---
+++ Ji-Do/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ji-Do
+//
+//  Created by 송영훈 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ji-Do/Ji_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ji_DoApp.swift
+//  Ji-Do
+//
+//  Created by 송영훈 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Ji_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
